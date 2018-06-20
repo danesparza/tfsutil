04873f959db62a65535ea4f56f843d63e23c600f
Dan Esparza <esparza.dan@gmail.com>
2018-06-20 13:02:07 -0400

    Move additional checks to 'vg' level

--- cmd/vg.go
+++ cmd/vg.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // vgCmd represents the variable group base command
@@ -15,6 +19,20 @@ var vgCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		//	This command on it's own should just show help
 		cmd.Help()
+	},
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+
+		//	Verify that we have a tfsurl and a pat
+		if strings.TrimSpace(viper.GetString("tfsurl")) == "" {
+			fmt.Printf("\nThis tool requires a TFS base url to operate.   \n\nPlease specify one on the command line or in the config file 'tfsutil.yml' \nFor help creating a config file, see the command 'tfsutil config create'\n")
+			os.Exit(1)
+		}
+
+		if strings.TrimSpace(viper.GetString("pat")) == "" {
+			fmt.Printf("\nThis tool requires a TFS Personal Access Token (pat) for authentication.  \n\nPlease specify a pat on the command line or in the config file 'tfsutil.yml' \nFor help creating a config file, see the command 'tfsutil config create'\n")
+			os.Exit(1)
+		}
+
 	},
 }
 
